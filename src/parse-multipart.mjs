f25373b1e0ba3a369899f6c3097d25ff564039e4
Ivan Vasilev <vianomore@gmail.com>
2023-08-23 18:14:36 +0200

    fix: multipart split

--- src/parse-multipart.mjs
+++ src/parse-multipart.mjs
@@ -1,16 +1,14 @@
 export function parseMultipart(body, contentType) {
-  console.log(body, contentType);
   const boundary = contentType.split("; ")[1].replace("boundary=", "");
-  const boundaryRegex = new RegExp(`-{0,2}${boundary}-{0,2}`);
+  const boundaryRegex = new RegExp(`-{0,2}${boundary}-{0,2}[\r\n]*`);
   const bodyParts = body.split(boundaryRegex).filter(Boolean);
-  console.log(bodyParts)
+
   if (bodyParts.length > 1) {
     throw new Error("Too many data props received.");
   }
   const [fileData] = bodyParts;
   const [, filename] = fileData.match(/Content-Disposition: form-data; name=".+"; filename="(.+)"/)
   const [, fileContentType] = fileData.match(/Content-Type: (.+)/)
-
 
   const file = fileData.replace(/[\r\n]*Content-Disposition: form-data; name=".+"; filename="(.+)"[\r\n]Content-Type: (.+)[\r\n]{1,2}/, '');
 
