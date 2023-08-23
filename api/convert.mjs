0c587ff0fc89e124312ee353b3e39799bc310798
Ivan Vasilev <vianomore@gmail.com>
2023-08-23 23:19:38 +0200

    feat: send response file

--- api/convert.mjs
+++ api/convert.mjs
@@ -1,13 +1,15 @@
 import * as xls from '../src/xls.mjs';
 import * as xml from '../src/xml.mjs';
-import { parseMultipart } from '../src/parse-multipart.mjs';
+import { getFile } from '../src/process-request.mjs';
 import { convertToSnowball } from '../src/parser.mjs';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(501).json({
+    res.statusCode = 501;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify({
       message: 'Not implemented.'
-    });
+    }));
   }
 
   let body = [];
@@ -18,12 +20,14 @@ export default async function handler(req, res) {
   req.on('end', async () => {
     let fileData = null;
     try {
-      fileData = parseMultipart(Buffer.concat(body), req.headers['content-type']);
+      fileData = getFile(Buffer.concat(body), req.headers['content-type']);
     } catch (err) {
       console.log(err);
-      return res.status(400).json({
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'application/json');
+      return res.end(JSON.stringify({
         message: err.message
-      });
+      }));
     }
 
     let report = null;
@@ -37,12 +41,17 @@ export default async function handler(req, res) {
         report = await xml.parseReport(data);
         break;
       default:
-        return res.status(400).json({
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify({
           message: 'Report could not be parsed. Unsupported file type.'
-        });
+        }));
     };
+
     const converted = convertToSnowball(report);
-    const csvFile = xls.saveReport(converted);
-    return res.status(200).send(csvFile);
+    const xlsReportFile = xls.saveReport(converted);
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    res.end(xlsReportFile);
   });
 }
---
+++ src/process-request.mjs
@@ -0,0 +1,8 @@
+import { parseMultipart } from './parse-multipart.mjs';
+
+export function getFile(body, contentType) {
+  if (!contentType.includes('multipart/form-data')) {
+    throw new Error("Unsupported content type.");
+  }
+  return parseMultipart(Buffer.concat(body),);
+}
