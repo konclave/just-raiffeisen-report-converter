07557b1b59f8b0c7a5cec56749822970cb6c054b
Ivan Vasilev <vianomore@gmail.com>
2023-08-23 18:14:45 +0200

    fix: dev server

--- src/server.mjs
+++ src/server.mjs
@@ -1,23 +1,14 @@
 /* Server for local testing perposes */
 import http from 'http';
-import { handleRequest } from './handler.mjs';
+import handler from '../api/convert.mjs';
+import { readFileSync } from 'fs';
 
 const server = http.createServer();
 server.on('request', (request, response) => {
-  let body = [];
-  request.on('data', (chunk) => {
-    console.log('chunk', chunk);
-    if (chunk instanceof Buffer) {
-      body.push(chunk);
-    }
-  }).on('end', () => {
-    body = Buffer.concat(body);
-    const { headers, method, url } = request;
-    const data = handleRequest({ headers, method, url, body });
-    response.writeHead(200, {
-      'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-      'Content-Disposition': 'attachment; filename="snowball_report.xlsx"'
-    });
-    response.end(data);
-  });
+  if (request.method === 'GET') {
+    const html = readFileSync('./public/index.html', 'utf8');
+    response.end(html);
+    return;
+  }
+  handler(request, response);
 }).listen(8080);
